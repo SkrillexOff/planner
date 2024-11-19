c4960095de1d3bd5a5bbe4ddaf0a0283a15a97f7
SkrillexOff <kulaevstas@yandex.ru>
2024-11-19 11:23:56 +0400

    6

--- auth.js
+++ auth.js
@@ -41,7 +41,7 @@ document.addEventListener('DOMContentLoaded', async () => {
             } catch (error) {
                 alert('Ошибка при входе: ' + error);
 
-                if (error.code === 'auth/invalid-login-credentials') {
+                if (error.message.includes('user-not-found')) {
                     alert('Пользователь не найден, регистрируем...');
                     // Если пользователя нет, регистрируем его
                     try {
